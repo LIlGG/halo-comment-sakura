769cae70fb72d0d0affd79e30622dc1bc5a72f3e
lxy <1103069291@qq.com>
2020-07-06 22:10:10 +0800

    fix #3 修复由于内联样式问题，导致评论提醒报错的BUG

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-debugger': 0,
         // 'no-unused-vars': 0
     },
     parserOptions: {
--- src/plugins/Tips/index.vue
+++ src/plugins/Tips/index.vue
@@ -1,5 +1,5 @@
 <template>
-<div v-show="show" class="butterBar butterBar--center">
+<div v-show="show" class="butterBar butterBar-center">
     <p class="butterBar-message" v-html="message">
     </p>
     </div>
@@ -21,32 +21,3 @@ export default {
     }
 }
 </script>
-
-<style lang="scss" scoped>
-.butterBar {
-    position: fixed;
-    text-align: center;
-    top: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    z-index: 10000
-}
-
-.butterBar--center {
-    margin: auto
-}
-
-.butterBar-message {
-    background: #FE9600;
-    color: #fff;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    display: inline-block;
-    font-size: 14px;
-    margin-bottom: 0;
-    padding: 12px 25px;
-    z-index: 10000;
-    margin-top: -4px
-}
-</style>
